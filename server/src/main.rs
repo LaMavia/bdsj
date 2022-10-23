053880e57dfe5318b6667c6cd11e4938c73fe076
LaMavia <tosurowiec@gmail.com>
2022-10-23 23:18:48 +0200

    change placeholder

--- server/src/main.rs
+++ server/src/main.rs
@@ -24,13 +24,17 @@
 
 use std::str::from_utf8;
 
-use cgi::{text_response, Request};
+use cgi::{html_response, Request};
 
 extern crate cgi;
 
 cgi::cgi_try_main! {
     |req: Request| -> Result<cgi::Response, String> {
         let body = from_utf8(req.body().as_slice()).map_err(|e| e.to_string())?;
-        Ok(text_response(200, body))
+        Ok(html_response(200, format!("
+        <h1>Hello there</h1>
+        <p>This appears to be a website :)</p>
+        <pre>{}</pre>
+        ", body)))
     }
 }
