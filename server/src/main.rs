86bf14f9b9af09b7afd3d9ab21ae9601504047bc
LaMavia <tosurowiec@gmail.com>
2022-10-23 23:34:50 +0200

    change placeholder

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,40 +1,25 @@
-// use std::path::{Path, PathBuf};
-
-// use rocket::{fs::NamedFile, response::Redirect};
-
-// #[macro_use]
-// extern crate rocket;
-
-// #[get("/<file..>")]
-// async fn files(file: PathBuf) -> Option<NamedFile> {
-//     NamedFile::open(Path::new("../client/dist/").join(file))
-//         .await
-//         .ok()
-// }
-
-// #[get("/")]
-// async fn index() -> Redirect {
-//     Redirect::to(uri!("/index.html"))
-// }
-
-// #[launch]
-// fn rocket() -> _ {
-//     rocket::build().mount("/", routes![index, files])
-// }
-
 use std::str::from_utf8;
 
 use cgi::{html_response, Request};
+use http::{header::HeaderName, HeaderValue};
 
 extern crate cgi;
 
 cgi::cgi_try_main! {
     |req: Request| -> Result<cgi::Response, String> {
         let body = from_utf8(req.body().as_slice()).map_err(|e| e.to_string())?;
+        let method = req.method();
+        let headers = req.headers();
+
+
         Ok(html_response(200, format!("
         <h1>Hello there</h1>
         <p>This appears to be a website :)</p>
-        <pre>{}</pre>
-        ", body)))
+        <span>method: {m}</span>
+        <span>headers:</span>
+        <pre>{hs:?}</pre>
+        <span>body:</span>
+        <pre>{b}</pre>
+        ", m=method, hs=headers.iter().collect::<Vec<(&HeaderName, &HeaderValue)>>(), b=body)))
     }
 }
